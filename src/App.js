e0dfea005256373f072021229183a85f85463eda
Sanket Marathe <sanketmarathe2001@gmail.com>
2021-10-16 21:01:21 +0530

    fetch notes

--- src/App.js
+++ src/App.js
@@ -6,27 +6,31 @@ import {
 } from "react-router-dom";
 import Navbar from './components/Navbar';
 import { Home } from './components/Home';
-import About from './components/About'; 
+import About from './components/About';
 import NoteState from './context/notes/NoteState';
+import { Alert } from './components/Alert';
 
 
 function App() {
   return (
-    <> 
-    <NoteState>
-      <Router>
-        <Navbar /> 
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about">
-            <About/>
-          </Route> 
-        </Switch>
-      </Router>
-    </NoteState>
-  </>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert message="This is amazing React course" />
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </NoteState>
+    </>
   );
 }
 
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,22 +2,30 @@ import NoteContext from "./NoteContext";
 import { useState } from "react";
 
 const NoteState = (props)=>{
-    const s1 = {
-        "name": "Harry",
-        "class": "5b"
-    }
-    const [state, setState] = useState(s1);
 
-    const update = ()=>{
-        setTimeout(() => {
-            setState({
-                "name": "Larry",
-                "class": "10b"
-            })
-        }, 1000);
-    }
+    const notesInitial =[
+            {
+              "_id": "61595e88eade6a9154ec039c",
+              "user": "6155cd35e351cd66258fdfa0",
+              "title": "My Title 2",
+              "description": "First Note by me",
+              "tag": "personal",
+              "date": "2021-10-03T07:40:56.829Z",
+              "__v": 0
+            },
+            {
+              "_id": "616ae9f84ceb8bf3862bb565",
+              "user": "6155cd35e351cd66258fdfa0",
+              "title": "My Title",
+              "description": "description",
+              "tag": "personal",
+              "date": "2021-10-16T15:04:24.873Z",
+              "__v": 0
+            }
+          ]
+    const [notes, setNotes] = useState(notesInitial)
     return (
-        <NoteContext.Provider value={{state:state, update:update}}>
+        <NoteContext.Provider value={{notes,setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
